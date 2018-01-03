83af122273a8bb42af5fa60c74ba9f453347c5cb
Elly Zhu <ezhu0808@gmail.com>
2018-01-04 10:32:12 +1100

    add emoji, change the nav bar list text to be more specific

--- app.js
+++ app.js
@@ -7,8 +7,14 @@
 
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 
 app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({
+   extended: false
+}));
+
 
 //Homepage
 app.get('/',function(req,res){
