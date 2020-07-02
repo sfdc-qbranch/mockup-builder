5e745ce8204eddeaacde1d7181dae776a96dc265
Elly Zhu <ezhu0808@gmail.com>
2020-07-03 01:22:56 +1000

    add youtube video on home page

---
+++ public/js/pages/home.js
@@ -0,0 +1,58 @@
+$(document).ready(function () {
+
+    var tag = document.createElement('script');
+    tag.id = 'iframe-demo';
+    tag.src = 'https://www.youtube.com/iframe_api';
+    var firstScriptTag = document.getElementsByTagName('script')[0];
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+    var player;
+
+    function onYouTubeIframeAPIReady() {
+        player = new YT.Player('youtube-iframe', {
+            events: {
+                'onReady': onPlayerReady,
+                'onStateChange': onPlayerStateChange
+            }
+        });
+    }
+
+    function onPlayerReady(event) {
+
+    }
+
+    function changeBorderColor(playerStatus) {
+        var color;
+        if (playerStatus == -1) {
+            color = "#37474F"; // unstarted = gray
+        } else if (playerStatus == 0) {
+            color = "#FFFF00"; // ended = yellow
+        } else if (playerStatus == 1) {
+            color = "#33691E"; // playing = green
+        } else if (playerStatus == 2) {
+            color = "#DD2C00"; // paused = red
+        } else if (playerStatus == 3) {
+            color = "#AA00FF"; // buffering = purple
+        } else if (playerStatus == 5) {
+            color = "#FF6DOO"; // video cued = orange
+        }
+        if (color) {
+            document.getElementById('youtube-iframe').style.borderColor = color;
+        }
+    }
+
+    function onPlayerStateChange(event) {
+        changeBorderColor(event.data);
+    }
+
+    $('#btn-openModal').click(function () {
+        $('#qMB_IntroVideo').show();
+    });
+
+    $('#btn-closeModal').click(function () {
+        $('.youtubevideo').each(function () {
+            this.contentWindow.postMessage('{"event":"command","func":"stopVideo","args":""}', '*')
+        });
+        $('#qMB_IntroVideo').hide();
+    });
+})
